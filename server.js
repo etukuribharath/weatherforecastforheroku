1bb7378523317d874754da0d6e348ba7091c3f46
Etukuri Bharath <etukuri.bharath123@gmail.com>
2016-11-08 04:43:17 +0530

    Testing

---
+++ server.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var express = require('express');
+var path = require("path");
+var PORT = process.env.PORT || 8080
+
+
+
+app.use(express.static(__dirname + '/../dev'));
+
+
+
+
+http.listen(PORT, function(){
+ console.log('listening on *:8080');
+});
